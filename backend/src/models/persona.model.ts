da6f17df2a9be41912b87fe104c7ce94eb279b53
jakebildy <jbildy@uwo.ca>
2024-03-07 21:12:56 -0800

    :sparkles: getting closer

--- backend/src/models/persona.model.ts
+++ backend/src/models/persona.model.ts
@@ -8,23 +8,54 @@ export interface UserPersona {
   }
   
 
-  const PersonaSchema = new mongoose.Schema<UserPersona>(
+const PersonaSchema = new mongoose.Schema<UserPersona>(
+{
+    id: { type: String, required: true },
+    name: { type: String, required: true },
+    pictureURL: { type: String, required: true },
+    sections: [
+        {
+        label: { type: String, required: true },
+        description: { type: String, required: true },
+        },
+    ],
+},
+{
+    versionKey: false,
+    timestamps: true,
+},
+);
+
+export const Persona = mongoose.model("Persona", PersonaSchema);
+
+
+//   PersonaHistory object - contains the message history, AI suggested messages, and the User Persona
+
+export interface PersonaHistory {
+    messageHistory: [{sender: "bot"|"user", text: string}]
+    persona?: UserPersona
+    aiSuggestedChats?: string[]
+    user?: string
+}
+
+const PersonaHistorySchema = new mongoose.Schema<PersonaHistory>(
     {
-        id: { type: String, required: true },
-        name: { type: String, required: true },
-        pictureURL: { type: String, required: true },
-        sections: [
-            {
-            label: { type: String, required: true },
-            description: { type: String, required: true },
-            },
-        ],
+    messageHistory: [
+        {
+        sender: { type: String, required: true },
+        text: { type: String, required: true },
+        },
+    ],
+    persona: { type: PersonaSchema, required: false },
+    aiSuggestedChats: [{ type: String, required: false }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     },
     {
-      versionKey: false,
-      timestamps: true,
+        versionKey: false,
+        timestamps: true,
     },
-  );
-  
-  export const Persona = mongoose.model("Persona", PersonaSchema);
-  
+    );
+    
+export const PersonaHistory = mongoose.model("PersonaHistory", PersonaHistorySchema);
+    
+    
