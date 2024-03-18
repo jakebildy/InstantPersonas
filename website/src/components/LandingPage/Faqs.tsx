a1dde68f014b19150771ff3b27a25758b578a701
jakebildy <jbildy@uwo.ca>
2024-03-18 15:06:35 -0700

    :fire: sexy login page

--- website/src/components/LandingPage/Faqs.tsx
+++ website/src/components/LandingPage/Faqs.tsx
@@ -1,6 +1,8 @@
 // import { Container } from './Container'
 import backgroundImage from "../../images/background-faqs.jpg";
 import { Container } from "../Container";
+import { BackgroundGradient } from "../ui/card_background_gradient";
+import { BackgroundGradientAnimation } from "../ui/gradient_background";
 
 const faqs = [
   [
@@ -31,7 +33,7 @@ const faqs = [
     {
       question: "How does InstantPersonas.com save me time?",
       answer:
-        "InstantPersonas.com  uses AI-powered technology to instantly generate SWOT analyses, PESTEL analyses, Lean Canvases, and User Personas from just the description of a product or service, which can be customized after.",
+        "InstantPersonas.com  uses AI-powered technology to instantly generate User Personas. Just by talking to our AI, you're able to figure out gaps in your understanding of your target market, and adjust accordingly.",
     },
   ],
 ];
@@ -41,48 +43,47 @@ export function Faqs() {
     <section
       id="faq"
       aria-labelledby="faq-title"
-      className="relative overflow-hidden bg-slate-50 py-20 sm:py-32"
+      className="relative overflow-hidden bg-white "
     >
-      <img
-        className="absolute left-1/2 top-0 max-w-none -translate-y-1/4 translate-x-[-30%]"
-        src={backgroundImage}
-        alt=""
-        width={1558}
-        height={946}
-      />
-      <Container className="relative">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2
-            id="faq-title"
-            className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl"
+      <BackgroundGradientAnimation>
+        <Container className="relative mt-20">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2
+              id="faq-title"
+              className="font-display text-3xl tracking-tight text-white font-bold sm:text-4xl"
+            >
+              Frequently Asked Questions
+            </h2>
+            <p className="mt-4 text-lg tracking-tight text-slate-100">
+              If you can’t find what you’re looking for, email our support team
+              and someone will get back to you.
+            </p>
+          </div>
+          <ul
+            role="list"
+            className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3"
           >
-            Frequently Asked Questions
-          </h2>
-          <p className="mt-4 text-lg tracking-tight text-slate-700">
-            If you can’t find what you’re looking for, email our support team
-            and someone will get back to you.
-          </p>
-        </div>
-        <ul
-          role="list"
-          className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 lg:max-w-none lg:grid-cols-3"
-        >
-          {faqs.map((column, columnIndex) => (
-            <li key={columnIndex}>
-              <ul role="list" className="flex flex-col gap-y-8">
-                {column.map((faq, faqIndex) => (
-                  <li key={faqIndex}>
-                    <h3 className="font-display text-lg leading-7 text-slate-900">
-                      {faq.question}
-                    </h3>
-                    <p className="mt-4 text-sm text-slate-700">{faq.answer}</p>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      </Container>
+            {faqs.map((column, columnIndex) => (
+              <li key={columnIndex}>
+                <ul role="list" className="flex flex-col gap-y-8 ">
+                  {column.map((faq, faqIndex) => (
+                    <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+                      <li key={faqIndex}>
+                        <h3 className="font-display text-lg leading-7 text-black">
+                          {faq.question}
+                        </h3>
+                        <p className="mt-4 text-sm text-slate-900">
+                          {faq.answer}
+                        </p>
+                      </li>
+                    </BackgroundGradient>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </Container>
+      </BackgroundGradientAnimation>
     </section>
   );
 }
--- website/src/components/LandingPage/PrimaryFeatures.tsx
+++ website/src/components/LandingPage/PrimaryFeatures.tsx
@@ -1,47 +1,9 @@
-import { useEffect, useRef, useState } from "react";
-import { Tab } from "@headlessui/react";
-import clsx from "clsx";
-
 import { Container } from "../Container";
-import swot from "../../images/screenshots/swot.png";
-import pestel from "../../images/screenshots/pestel.png";
-import leanCanvas from "../../images/screenshots/lean-canvas.png";
 import userPersona from "../../images/screenshots/user-persona.png";
-import backgroundImage from "../../images/background-analyses-lg.jpg";
-import { useInView } from "framer-motion";
 import { AuroraBackground } from "../ui/aurora_background";
 import { MacbookScroll } from "../ui/macbook_scroll";
 
-const features = [
-  {
-    title: "SWOT Analyses",
-    description:
-      "AI-powered tool to create a SWOT Analysis for your company based on its description. A SWOT analysis is a strategic planning tool that helps to identify a business's internal strengths and weaknesses, as well as external opportunities and threats.",
-    image: swot,
-  },
-  {
-    title: "User Personas",
-    description:
-      "Powerful and customizable tool that uses AI to generate detailed user personas for businesses and organizations. A user persona is a fictional character that represents a company's ideal customer, based on market research and customer data.",
-    image: userPersona,
-  },
-  {
-    title: "Lean Canvases",
-    description:
-      "Create comprehensive Lean Canvases effortlessly based on the description of a business. A Lean Canvas is a one page business plan.",
-    image: leanCanvas,
-  },
-  {
-    title: "PESTEL Analyses",
-    description:
-      "Our AI-powered tool lets you quickly create a PESTEL Analysis for your company based on its description. A PESTEL analysis is a strategic tool used to identify and analyze the external factors that can impact an organization's business operations and decision-making.",
-    image: pestel,
-  },
-];
-
 export function PrimaryFeatures() {
-  const [tabOrientation, setTabOrientation] = useState("horizontal");
-
   // const ref = useRef(null);
   // const isInView = useInView(ref, { once: true });
 
