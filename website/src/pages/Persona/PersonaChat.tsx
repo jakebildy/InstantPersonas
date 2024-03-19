38282ea3823dcbbc9658fbc2306f1213978bad8a
Zero <writemarcuslim@gmail.com>
2024-03-19 16:16:08 -0400

    feat(personachat): check subscription dialog

--- website/src/pages/Persona/PersonaChat.tsx
+++ website/src/pages/Persona/PersonaChat.tsx
@@ -11,6 +11,9 @@ import { RefObject, useRef, useState } from "react";
 import { ColorResult, CompactPicker } from "react-color";
 import { generateTimestamp } from "@/lib/utils";
 import { toPng } from "html-to-image";
+import { Dialog } from "@/components/ui/dialog";
+import { SubscriptionTrialDialog } from "./SubscriptionTrialDialog";
+import { useUser } from "@/contexts/UserContext";
 
 export const PersonaChat = () => {
   const { id } = useGetPersonaPathId();
@@ -31,6 +34,8 @@ export const PersonaChat = () => {
   } = usePersonaChat(id);
   const personaRef = useRef<HTMLDivElement>(null);
   const [showPicker, setShowPicker] = useState<boolean>(false);
+  const [showSubscriptionPromptDialog, setShowSubscriptionPromptDialog] =
+    useState<boolean>(false);
   const renderPersona = !personaHasNoValues(persona);
 
   const downloadImage = (elementRef: RefObject<HTMLElement>) => {
@@ -41,95 +46,117 @@ export const PersonaChat = () => {
       download(dataUrl, "Persona-" + timeStamp + ".png");
     });
   };
+  const { subscriptionActive } = useUser();
+
+  const handleCheckSubscriptionSubmit = (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    if (!subscriptionActive) {
+      setShowSubscriptionPromptDialog(true);
+    } else {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <Sidebar currentSelectedPage="Persona Creator">
-      <div className="flex flex-col relative">
-        {renderPersona ? (
-          <div className="flex-1 flex flex-col">
-            <div ref={personaRef}>
-              <UserPersona {...{ selectedColor, setPersona, id, ...persona }} />
-            </div>
-            <div className="flex gap-4 lg:gap-8 my-4 overflow-hidden flex-wrap justify-center">
-              <Button onClick={() => setShowPicker(!showPicker)}>
-                {"Change Colour"}
-              </Button>
-              {showPicker && (
-                <div style={{ position: "relative" }}>
-                  <CompactPicker
-                    color={selectedColor}
-                    onChangeComplete={(color: ColorResult) => {
-                      setSelectedColor(color.hex);
-                      setShowPicker(false);
-                      updateColor(color.hex);
-                    }}
-                  />
-                </div>
-              )}
-              <Button
-                onClick={() => {
-                  const newPicture = `https://xsgames.co/randomusers/assets/avatars/${persona.gender.toLowerCase()}/${Math.ceil(
-                    Math.random() * 78
-                  )}.jpg`;
+      <Dialog
+        open={showSubscriptionPromptDialog}
+        onOpenChange={setShowSubscriptionPromptDialog}
+      >
+        <div className="flex flex-col relative">
+          {renderPersona ? (
+            <div className="flex-1 flex flex-col">
+              <div ref={personaRef}>
+                <UserPersona
+                  {...{ selectedColor, setPersona, id, ...persona }}
+                />
+              </div>
+              <div className="flex gap-4 lg:gap-8 my-4 overflow-hidden flex-wrap justify-center">
+                <Button onClick={() => setShowPicker(!showPicker)}>
+                  {"Change Colour"}
+                </Button>
+                {showPicker && (
+                  <div style={{ position: "relative" }}>
+                    <CompactPicker
+                      color={selectedColor}
+                      onChangeComplete={(color: ColorResult) => {
+                        setSelectedColor(color.hex);
+                        setShowPicker(false);
+                        updateColor(color.hex);
+                      }}
+                    />
+                  </div>
+                )}
+                <Button
+                  onClick={() => {
+                    const newPicture = `https://xsgames.co/randomusers/assets/avatars/${persona.gender.toLowerCase()}/${Math.ceil(
+                      Math.random() * 78
+                    )}.jpg`;
 
-                  updatePicture(newPicture);
-                  setPersona({
-                    ...persona,
-                    pictureURL: newPicture,
-                  });
-                }}
-              >
-                {"Change Picture"}
-              </Button>
-              <Button onClick={() => downloadImage(personaRef)}>
-                {"Download Image"}
-              </Button>
-            </div>
-          </div>
-        ) : null}
-        <div
-          className={cn(
-            "flex-1",
-            renderPersona ? "min-h-[35dvh] lg:min-h-[10dvh]" : ""
-          )}
-          id="scroll-spacer"
-        />
-        {/* 288px is w-72 the sidebar lg width */}
-        <div className="fixed bottom-0 w-full lg:w-[calc(100%-288px)]">
-          {/* 18px is padding + margin of the chat component */}
-          {/* 78px: 18px + 60px is the height of margin + mobile header */}
-          <Chat
-            className={cn(
-              "border rounded-lg bg-white shadow-xl",
-              renderPersona
-                ? "min-h-[400px]"
-                : "min-h-[calc(100dvh-78px)] lg:min-h-[calc(100vh-18px)]"
-            )}
-            messages={messages}
-            handleSubmit={handleSubmit}
-            handleInputChange={(e) => setInput(e.target.value)}
-            input={input}
-            loading={loading}
-            setLoading={(b) => setLoading(b)}
-          >
-            <div className="flex flex-col flex-wrap ">
-              <div className="flex gap-4 my-4 overflow-hidden flex-wrap">
-                {suggestions.map((suggestion, i) => (
-                  <Button
-                    key={i}
-                    variant={"secondary"}
-                    className="text-white bg-gray-400/75 rounded-lg text-xs hover:bg-green-400"
-                    type="submit"
-                    onClick={() => setInput(suggestion)}
-                  >
-                    {suggestion}
-                  </Button>
-                ))}
+                    updatePicture(newPicture);
+                    setPersona({
+                      ...persona,
+                      pictureURL: newPicture,
+                    });
+                  }}
+                >
+                  {"Change Picture"}
+                </Button>
+                <Button onClick={() => downloadImage(personaRef)}>
+                  {"Download Image"}
+                </Button>
               </div>
             </div>
-          </Chat>
+          ) : null}
+          <div
+            className={cn(
+              "flex-1",
+              renderPersona ? "min-h-[35dvh] lg:min-h-[10dvh]" : ""
+            )}
+            id="scroll-spacer"
+          />
+          {/* 288px is w-72 the sidebar lg width */}
+          <div className="fixed bottom-0 w-full lg:w-[calc(100%-288px)]">
+            {/* 18px is padding + margin of the chat component */}
+            {/* 78px: 18px + 60px is the height of margin + mobile header */}
+            <Chat
+              className={cn(
+                "border rounded-lg bg-white shadow-xl",
+                renderPersona
+                  ? "min-h-[400px]"
+                  : "min-h-[calc(100dvh-78px)] lg:min-h-[calc(100vh-18px)]"
+              )}
+              messages={messages}
+              handleSubmit={handleCheckSubscriptionSubmit}
+              handleInputChange={(e) => setInput(e.target.value)}
+              input={input}
+              loading={loading}
+              setLoading={(b) => setLoading(b)}
+            >
+              <div className="flex flex-col flex-wrap ">
+                <div className="flex gap-4 my-4 overflow-hidden flex-wrap">
+                  {suggestions.map((suggestion, i) => (
+                    <Button
+                      key={i}
+                      variant={"secondary"}
+                      className="text-white bg-gray-400/75 rounded-lg text-xs hover:bg-green-400"
+                      type="submit"
+                      onClick={() => setInput(suggestion)}
+                    >
+                      {suggestion}
+                    </Button>
+                  ))}
+                </div>
+              </div>
+            </Chat>
+          </div>
         </div>
-      </div>
+        <SubscriptionTrialDialog
+          onCloseAutoFocus={() => personaRef.current?.focus()}
+        />
+      </Dialog>
     </Sidebar>
   );
 };
---
+++ website/src/pages/Persona/SubscriptionTrialDialog.tsx
@@ -0,0 +1,63 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { DialogOverlay, DialogPortal } from "@/components/ui/dialog";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+export const SubscriptionTrialDialog = React.forwardRef<
+  React.ElementRef<typeof DialogPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => {
+  const navigate = useNavigate();
+  return (
+    <DialogPortal>
+      <DialogOverlay className="bg-black/75" />
+      <DialogPrimitive.Content
+        className={cn(
+          "fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
+          className
+        )}
+        ref={ref}
+        {...props}
+      >
+        <div className="grid place-items-center md:grid-cols-5 gap-4">
+          <div className="w-[200px] h-[200px] md:col-span-2">
+            <img
+              src="/affiliate.jpg"
+              alt="free subscription"
+              className="object-contain"
+            />
+          </div>
+          <div className="space-y-4 md:col-span-3">
+            <h2 className="text-lg font-bold text-gray-800">
+              Take Your Chats Further: Try Persona Chat's Premium Access!
+            </h2>
+            <p className="text-sm text-gray-600">
+              To continue using Persona Chat, you need to subscribe to our
+              service, or check out our free 3 week trial.
+            </p>
+            <div className="flex space-x-4">
+              <DialogPrimitive.Close asChild>
+                <Button variant={"secondary"}>Close</Button>
+              </DialogPrimitive.Close>
+              <Button
+                className="px-6"
+                onClick={() => navigate("/subscription")}
+              >
+                Try Now!
+              </Button>
+            </div>
+          </div>
+        </div>
+        {children}
+        <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </DialogPrimitive.Close>
+      </DialogPrimitive.Content>
+    </DialogPortal>
+  );
+});
+SubscriptionTrialDialog.displayName = "SubscriptionTrialDialog";
