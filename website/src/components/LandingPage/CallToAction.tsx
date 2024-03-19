b1b9c744ac0e300fba0a6d3f4e1fe45a11626448
jakebildy <jbildy@uwo.ca>
2024-03-19 16:04:24 -0700

    :fire: tweaks to the theme

--- website/src/components/LandingPage/CallToAction.tsx
+++ website/src/components/LandingPage/CallToAction.tsx
@@ -1,33 +1,83 @@
 // import { Button } from "../Button";
 import { Container } from "../Container";
+import { AnimatedTooltip } from "../ui/animated_tooltip";
 import { LampContainer } from "../ui/lamp";
-import { Button } from "../ui/moving_border";
 
 export function CallToAction() {
+  const people = [
+    {
+      id: 1,
+      name: "Marcus (Persona)",
+      designation: "Software Engineer",
+      image:
+        "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    },
+    {
+      id: 2,
+      name: "Joe (Persona)",
+      designation: "Product Manager",
+      image:
+        "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 3,
+      name: "Jane (Persona)",
+      designation: "TikTok Influencer",
+      image:
+        "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 4,
+      name: "Emily (Persona)",
+      designation: "UX Designer",
+      image:
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    {
+      id: 5,
+      name: "Tyler (Persona)",
+      designation: "Kombucha Brewer",
+      image:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    },
+    {
+      id: 6,
+      name: "Dora (Persona)",
+      designation: "The Explorer",
+      image:
+        "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    },
+  ];
+
   return (
     <section
       id="get-started-today"
-      className="relative overflow-hidden bg-black py-32"
+      className="relative overflow-hidden bg-white py-32"
     >
-      <Container className="relative">
-        <LampContainer>
-          <div className="mx-auto max-w-lg text-center">
-            <h2 className="font-display text-3xl tracking-tight text-white sm:text-6xl font-bold">
-              Get started today
-            </h2>
-            <p className="mt-4 text-lg tracking-tight text-white">
-              <b>It’s time to take back your time.</b>
-            </p>
-            <p className="mt-4 text-lg tracking-tight text-white mb-10">
-              Save hours by generating in-depth personas effortlessly with
-              InstantPersonas.
-            </p>
-
-            <Button href="/register" color="white" className=" font-bold">
-              Get 3 days FREE
-            </Button>
+      <Container className="relative mt-[100px]">
+        <div className="mx-auto max-w-lg text-center">
+          <div className="flex flex-row items-center justify-center mb-10 w-full">
+            <AnimatedTooltip items={people} />
           </div>
-        </LampContainer>
+          <h2 className="font-display text-3xl tracking-tight text-black sm:text-6xl font-bold">
+            Get started today
+          </h2>
+          <p className="mt-4 text-lg tracking-tight text-black">
+            <b>It’s time to take back your time.</b>
+          </p>
+          <p className="mt-4 text-lg tracking-tight text-black mb-10">
+            Save hours by generating in-depth personas effortlessly with
+            InstantPersonas.
+          </p>
+
+          {/* <Button href="/register" color="white" className=" font-bold">
+              Get 3 days FREE
+            </Button> */}
+
+          <button className="px-8 py-2 rounded-full bg-gradient-to-b from-green-500 to-green-600 text-white focus:ring-2 focus:ring-green-400 hover:shadow-xl transition duration-200">
+            Get 3 days FREE
+          </button>
+        </div>
       </Container>
     </section>
   );
