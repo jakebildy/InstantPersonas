dc4a2fb8d24f847e1671a509912f10a7d3e911e8
Zero <writemarcuslim@gmail.com>
2024-03-19 20:10:26 -0400

    refactor(bot chat): rem to px

--- website/src/components/chat.tsx
+++ website/src/components/chat.tsx
@@ -112,14 +112,11 @@ const PersonaMessage = ({
   return (
     <div className={cn("flex gap-2 items-center", className)} {...Props}>
       {/* 32px + 16px = 48px ~ image width + gap */}
-      <div className="flex items-center h-full w-[calc(32px+16px)]">
+      <div className="flex items-center h-8 w-8">
         <img
           src={"/instant_personas_logo.png"}
           alt={"Instant Personas Logo"}
-          // width={32}
-          // height={28}
-          // priority
-          className={cn("object-contain w-8 h-7")}
+          className="object-contain min-w-[32px]"
         />
       </div>
       <p className="flex items-center bg-gray-200 p-2 px-4 rounded-lg text-sm whitespace-pre-wrap">
