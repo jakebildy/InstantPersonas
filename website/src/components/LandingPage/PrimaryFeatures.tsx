f28d7910a0111601c517dc1dc911e6c6d70269c7
jakebildy <jbildy@uwo.ca>
2024-03-20 12:17:50 -0700

    :fire: fixed macbook scroll mobile friendliness

--- website/src/components/LandingPage/PrimaryFeatures.tsx
+++ website/src/components/LandingPage/PrimaryFeatures.tsx
@@ -13,29 +13,57 @@ export function PrimaryFeatures() {
       aria-label="Features for running your books"
       className="relative overflow-hidden bg-black dark h-[200vh]"
     >
-      <AuroraBackground className="h-[200vh]">
-        <Container className=" b-28 pt-20 sm:py-32 ">
-          <div className="height-[400vh]">
-            <MacbookScroll
-              title={
-                <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
-                  <h2 className="font-display text-3xl font-bold tracking-tight text-white sm:text-4xl md:text-7xl pt-20">
-                    Reclaim your free time.
-                  </h2>
-                  <p className="mt-6 text-lg tracking-tight text-blue-100">
-                    Deep understanding of your target market with AI-Powered
-                    Personas.
-                  </p>
-                  <p className="mt-6 text-lg tracking-tight text-blue-100">
-                    Our current products
-                  </p>
-                </div>
-              }
-              // src={userPersona}
-            ></MacbookScroll>
+      {/* if sm, hide */}
+      <div className="hidden sm:block">
+        <AuroraBackground className="h-[200vh]">
+          <Container className=" b-28 pt-20 sm:py-32 ">
+            <div className="height-[400vh]">
+              <MacbookScroll
+                title={
+                  <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
+                    <h2 className="font-display text-3xl font-bold tracking-tight text-white sm:text-4xl md:text-7xl pt-20">
+                      Reclaim your free time.
+                    </h2>
+                    <p className="mt-6 text-lg tracking-tight text-blue-100">
+                      Deep understanding of your target market with AI-Powered
+                      Personas.
+                    </p>
+                    <p className="mt-6 text-lg tracking-tight text-blue-100">
+                      Our current products
+                    </p>
+                  </div>
+                }
+                // src={userPersona}
+              ></MacbookScroll>
+            </div>
+          </Container>
+        </AuroraBackground>
+      </div>
+
+      {/* if md, hide */}
+      <div className="sm:hidden">
+        <Container className="b-28 pt-20 sm:py-32">
+          <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
+            <h2 className="font-display text-3xl font-bold tracking-tight text-white sm:text-4xl md:text-7xl pt-20">
+              Reclaim your free time.
+            </h2>
+            <p className="mt-6 text-lg tracking-tight text-blue-100">
+              Deep understanding of your target market with AI-Powered Personas.
+            </p>
+            <p className="mt-6 text-lg tracking-tight text-blue-100  mb-5">
+              Our current products
+            </p>
+            <iframe
+              className="  z-10 h-96 w-[100%] rounded-lg inset-0"
+              src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+              title="Rick Astley - Never Gonna Give You Up (Official Music Video)"
+              // frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              // allowfullscreen
+            ></iframe>
           </div>
         </Container>
-      </AuroraBackground>
+      </div>
     </section>
   );
 }
