422a0d19ef0765f7cc4442cb36a2a8390f2d7a69
jakebildy <jbildy@uwo.ca>
2024-03-22 12:51:13 -0700

    :fire: fixed build error

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -7,19 +7,16 @@ export function Footer({ isBlog }: { isBlog?: boolean }) {
   return (
     <footer className="bg-slate-50">
       <Container>
-        {/* <div className="py-16">
+        <div className="py-16">
           <img src={logo} className="mx-auto h-10 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
               {isBlog ? null : <NavLink href="#features">Features</NavLink>}
               {isBlog ? null : <NavLink href="/blog">Blog</NavLink>}
               {isBlog ? null : <NavLink href="#pricing">Pricing</NavLink>}
-              <NavLink href="https://affiliates.reflio.com/invite/InstantPersonas">
-                Affiliate Program
-              </NavLink>
             </div>
           </nav>
-        </div> */}
+        </div>
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse justify-center ">
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
             Copyright &copy; {new Date().getFullYear()} InstantPersonas.com All
