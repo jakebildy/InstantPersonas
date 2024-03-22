81d3fa57a7ddb4df0bc0762a9d7257486acb17e0
jakebildy <jbildy@uwo.ca>
2024-03-22 12:48:54 -0700

    :fire: added tos link to footer

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer({ isBlog }: { isBlog?: boolean }) {
   return (
     <footer className="bg-slate-50">
       <Container>
-        <div className="py-16">
+        {/* <div className="py-16">
           <img src={logo} className="mx-auto h-10 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
@@ -19,14 +19,14 @@ export function Footer({ isBlog }: { isBlog?: boolean }) {
               </NavLink>
             </div>
           </nav>
-        </div>
+        </div> */}
         <div className="flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse justify-center ">
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
             Copyright &copy; {new Date().getFullYear()} InstantPersonas.com All
             rights reserved. By using this site, you agree to our{" "}
             <a
               style={{ fontWeight: "bold" }}
-              href="https://pastebin.com/raw/CfcqdFK5"
+              href="https://instantpersonas.com/terms-of-service"
             >
               Terms of Service
             </a>
