2cbeb73358d8206c1ce6c72ee496e97a26ab306f
jakebildy <jbildy@uwo.ca>
2024-03-22 16:49:54 -0700

    :fire: added clarity

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -6,9 +6,9 @@ import UserProvider from "./contexts/UserContext.tsx";
 import { StytchProvider } from "@stytch/react";
 import { StytchUIClient } from "@stytch/vanilla-js";
 import { NotificationProvider } from "./contexts/NotificationContext.tsx";
-// import { clarity } from "react-microsoft-clarity";
-// clarity.init('h7ebzk29j1');
-// console.log("Clarity initialized");
+import { clarity } from "react-microsoft-clarity";
+clarity.init("lkw9ytrajz");
+console.log("Clarity initialized");
 
 const stytch = new StytchUIClient(
   import.meta.env.VITE_STYTCH_PUBLIC_TOKEN as string
