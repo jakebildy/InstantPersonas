03b0b67211f48b4f06b190ed9890eca9a5ee0f5e
Zero <writemarcuslim@gmail.com>
2024-04-01 19:45:16 -0400

    feat(next/home): dev nav btn

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import LandingPage from "@/components/landing-page";
 
+const env = process.env.NEXT_PUBLIC_ENV;
+
 export default function Home() {
   return (
     <main className="relative overflow-x-hidden">
@@ -15,13 +17,16 @@ export default function Home() {
       <div className="w-full h-[100px] from-slate-50 to-transparent bg-gradient-to-t backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
       <LandingPage.footer className="from-slate-50 from-[33%] to-transparent bg-gradient-to-t absolute bottom-0 bg-transparent w-screen overflow-hidden backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
 
-      <Button
-        asChild
-        className="fixed top-0 translate-y-[100px] m-10 z-50"
-        variant={"destructive"}
-      >
-        <Link href={"/dashboard"}>DEV BTN {`=>`} Dashboard</Link>
-      </Button>
+      {/* Remove on PROD */}
+      {env && env === "dev" ? (
+        <Button
+          asChild
+          className="fixed top-0 translate-y-[100px] m-10 z-50"
+          variant={"destructive"}
+        >
+          <Link href={"/dashboard"}>DEV BTN {`=>`} Dashboard</Link>
+        </Button>
+      ) : null}
     </main>
   );
 }
