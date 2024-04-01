2e5c5be85cbfdea5515482075d1647cc6e6e10fc
Zero <writemarcuslim@gmail.com>
2024-04-01 19:41:36 -0400

    feat(next/home): gradient

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -12,7 +12,8 @@ export default function Home() {
       <LandingPage.callToAction />
       <LandingPage.pricing />
       <LandingPage.faqs />
-      <LandingPage.footer className="from-slate-50 to-transparent bg-gradient-to-t absolute bottom-0 bg-transparent w-screen overflow-hidden backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
+      <div className="w-full h-[100px] from-slate-50 to-transparent bg-gradient-to-t backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
+      <LandingPage.footer className="from-slate-50 from-[33%] to-transparent bg-gradient-to-t absolute bottom-0 bg-transparent w-screen overflow-hidden backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
 
       <Button asChild className="absolute top-0 m-10 " variant={"destructive"}>
         <Link href={"/dashboard"}>Dashboard</Link>
