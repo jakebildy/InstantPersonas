870549b1bf4c4e8ed8f68d523d5153ff22d394b4
Zero <writemarcuslim@gmail.com>
2024-04-01 19:43:23 -0400

    feat(next/home): dev nav btn

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -15,8 +15,12 @@ export default function Home() {
       <div className="w-full h-[100px] from-slate-50 to-transparent bg-gradient-to-t backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
       <LandingPage.footer className="from-slate-50 from-[33%] to-transparent bg-gradient-to-t absolute bottom-0 bg-transparent w-screen overflow-hidden backdrop-blur supports-[backdrop-filter]:bg-bg-gradient-to-t" />
 
-      <Button asChild className="absolute top-0 m-10 " variant={"destructive"}>
-        <Link href={"/dashboard"}>Dashboard</Link>
+      <Button
+        asChild
+        className="fixed top-0 translate-y-[100px] m-10 z-50"
+        variant={"destructive"}
+      >
+        <Link href={"/dashboard"}>DEV BTN {`=>`} Dashboard</Link>
       </Button>
     </main>
   );
