067a6efa9d9601f17454ccd4de00901334171e94
Zero <writemarcuslim@gmail.com>
2024-04-08 22:47:54 -0400

    fix(middleware): response

--- next/middleware.tsx
+++ next/middleware.tsx
@@ -3,9 +3,12 @@ import { NextResponse, NextRequest } from "next/server";
 const ENV = process.env.NEXT_PUBLIC_ENV;
 // Reroute on prod to home if trying to access /test
 export function middleware(request: NextRequest) {
+  const res = NextResponse.next();
   if (ENV == "dev") {
-    return request;
-  } else return NextResponse.redirect(new URL("/", request.url));
+    return res;
+  } else {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 }
 
 export const config = {
