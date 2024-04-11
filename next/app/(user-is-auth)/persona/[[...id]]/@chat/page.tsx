8b40dd51b725ab285189b12b81c4654e08bdcb02
Zero <writemarcuslim@gmail.com>
2024-04-11 17:09:04 -0400

    feat(persona): data fetch

--- next/app/(user-is-auth)/persona/[[...id]]/@chat/page.tsx
+++ next/app/(user-is-auth)/persona/[[...id]]/@chat/page.tsx
@@ -1,20 +1,29 @@
 "use server";
 import { AI } from "@/app/(server)/action";
+import { PersonaChat } from "@/app/(server)/models/personachat.model";
 import Chat from "@/components/chat";
+import { PersonaAvatarPopover } from "@/components/generative-ui/persona-avatar-popover";
+import api from "@/service/api.service";
 
 export default async function ChatPage({
   params,
 }: {
   params: { id?: string[] };
 }) {
-  console.log(params.id?.at(-1));
+  const id = params.id?.at(-1);
+  let data = null;
+
+  if (id) {
+    const personaModel: PersonaChat = await api.userPersona.getPersonaChat(id);
+    data = personaModel;
+  }
 
   return (
     <div className="flex flex-col h-[calc(100vh-58px)] justify-center">
       <div className="flex flex-row">
-        {/* {aiState.personas.map((persona: any) => {
-          return <PersonaAvatarPopover {...{ archetype: persona }} />;
-        })} */}
+        {aiState.personas.map((archetype: any, i) => {
+          return <PersonaAvatarPopover key={i} {...{ archetype: archetype }} />;
+        })}
       </div>
       <Chat className="border rounded-lg min-h-[400px]" />
     </div>
