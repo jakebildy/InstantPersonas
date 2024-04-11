b1ea3bfb7401d96160bb837c9d69aa5bdf192a9b
Zero <writemarcuslim@gmail.com>
2024-04-11 17:17:53 -0400

    feat(persona): data set

--- next/app/(user-is-auth)/persona/[[...id]]/@chat/page.tsx
+++ next/app/(user-is-auth)/persona/[[...id]]/@chat/page.tsx
@@ -4,6 +4,7 @@ import { PersonaChat } from "@/app/(server)/models/personachat.model";
 import Chat from "@/components/chat";
 import { PersonaAvatarPopover } from "@/components/generative-ui/persona-avatar-popover";
 import api from "@/service/api.service";
+import { useActions } from "ai/rsc";
 
 export default async function ChatPage({
   params,
@@ -11,17 +12,18 @@ export default async function ChatPage({
   params: { id?: string[] };
 }) {
   const id = params.id?.at(-1);
-  let data = null;
+  const { getCurrentAIState, setInitialAIState } = useActions<typeof AI>();
+  const aiState = getCurrentAIState();
 
   if (id) {
     const personaModel: PersonaChat = await api.userPersona.getPersonaChat(id);
-    data = personaModel;
+    setInitialAIState(personaModel.aiState);
   }
 
   return (
     <div className="flex flex-col h-[calc(100vh-58px)] justify-center">
       <div className="flex flex-row">
-        {aiState.personas.map((archetype: any, i) => {
+        {aiState.personas.map((archetype: any, i: number) => {
           return <PersonaAvatarPopover key={i} {...{ archetype: archetype }} />;
         })}
       </div>
