42d2ac79b9dd6d5b360a26741ec796da09ae5ed0
jakebildy <jbildy@uwo.ca>
2024-04-18 17:42:22 -0700

    :fire: fixed client/server issues

--- next/app/(user-is-auth)/subscription/page.tsx
+++ next/app/(user-is-auth)/subscription/page.tsx
@@ -1,12 +1,27 @@
-"use server";
+"use client";
 
 import api from "@/service/api.service";
 import ActiveSubscription from "./_components/(views)/active-subscription";
 import DashboardPricing from "./_components/(views)/pricing-page";
 import { useStytchUser } from "@stytch/nextjs";
-export default async function SubscriptionPage() {
-  // const { user } = useStytchUser();
-  // const userIsSubscribed = await api.stripe.isSubscriptionActive(user?.user_id as string);
-  const userIsSubscribed = true;
-  return userIsSubscribed ? <ActiveSubscription /> : <DashboardPricing />;
+import { useEffect } from "react";
+export default function SubscriptionPage() {
+  const { user } = useStytchUser();
+
+  let isSubscribed = false;
+
+  // useEffect
+  useEffect(() => {
+    if (!user) return;
+    // make async call to check if user is subscribed
+    const checkSubscription = async () => {
+      const userIsSubscribed = await api.stripe.isSubscriptionActive(
+        user?.user_id as string
+      );
+      isSubscribed = userIsSubscribed;
+    };
+    checkSubscription();
+  }, [user]);
+
+  return isSubscribed ? <ActiveSubscription /> : <DashboardPricing />;
 }
