7192a3eb9ecaee8bfc758c9834ac8fc1b7941b0a
Zero <writemarcuslim@gmail.com>
2024-04-23 17:06:37 -0400

    test(auth): this will break build

--- next/app/(auth)/auth/page.tsx
+++ next/app/(auth)/auth/page.tsx
@@ -1,6 +1,5 @@
 import Authenticate from "@/components/auth/stytch-auth";
 
 export default function AuthenticatePage() {
-  const test = true;
-  return test ? <div>auth....</div> : <Authenticate />;
+  return <Authenticate />;
 }
