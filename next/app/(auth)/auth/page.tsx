6a0d90e71677e85315d897a5932dbe6d281904c9
jakebildy <jbildy@uwo.ca>
2024-04-23 12:46:34 -0700

    :fire: fix

--- next/app/(auth)/auth/page.tsx
+++ next/app/(auth)/auth/page.tsx
@@ -1,6 +1,6 @@
+"use client";
 import dynamic from "next/dynamic";
 
-// Dynamically import the Authenticate component with SSR disabled
 //@ts-ignore
 const Authenticate = dynamic(() => import("@/components/auth/stytch-auth"), {
   ssr: false, // Disable server-side rendering for this component
