4cb1f8212397fba9eefd754bfd4d7a60adab2aad
jakebildy <jbildy@uwo.ca>
2024-04-25 20:49:55 -0400

    :fire: removing middleware

--- next/middleware.tsx
+++ next/middleware.tsx
@@ -1,16 +1,16 @@
-import { NextResponse, NextRequest } from "next/server";
+// import { NextResponse, NextRequest } from "next/server";
 
-const ENV = process.env.NEXT_PUBLIC_ENV;
-// Reroute on prod to home if trying to access /test
-export function middleware(request: NextRequest) {
-  const res = NextResponse.next();
-  if (ENV == "dev") {
-    return res;
-  } else {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-}
+// const ENV = process.env.NEXT_PUBLIC_ENV;
+// // Reroute on prod to home if trying to access /test
+// export function middleware(request: NextRequest) {
+//   const res = NextResponse.next();
+//   if (ENV == "dev") {
+//     return res;
+//   } else {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+// }
 
-export const config = {
-  matcher: "/test",
-};
+// export const config = {
+//   matcher: "/test",
+// };
