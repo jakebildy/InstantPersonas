90de1aa56b8748db98526674f85b9989c9b01d56
jakebildy <jbildy@uwo.ca>
2024-04-27 15:05:06 -0400

    :fire: fixed get persona chatt

--- next/app/(server)/api/(persona-crud)/get-persona-chat/route.ts
+++ next/app/(server)/api/(persona-crud)/get-persona-chat/route.ts
@@ -1,5 +1,7 @@
 import { PersonaChat } from "@/app/(server)/models/personachat.model";
 import { PERSONA_TEST_HISTORY } from "./test";
+import mongoose from "mongoose";
+import { initMongoDB } from "@/database/mongodb";
 
 export async function GET(req: Request) {
   // a user can only get their own persona chats, filter by the provided userID in the request
@@ -9,6 +11,8 @@ export async function GET(req: Request) {
   const chatID = url.searchParams.get("chatID");
 
   // Get MongoDB PersonaChats where user matches the provided userID
+
+  mongoose.connection.readyState === 1 ? console.log("Mongoose Connected") : await initMongoDB();
   const personaChat = await PersonaChat.findOne({ _id: chatID });
 
   return Response.json({
