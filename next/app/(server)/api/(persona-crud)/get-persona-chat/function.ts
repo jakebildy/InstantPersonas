818a3e473ec3ac1cba1812c09bfbd767d26313fe
Zero <writemarcuslim@gmail.com>
2024-04-27 17:02:14 -0400

    fix(get-persona-chat): fixed

---
+++ next/app/(server)/api/(persona-crud)/get-persona-chat/function.ts
@@ -0,0 +1,14 @@
+import "server-only";
+
+import { PersonaChat } from "@/app/(server)/models/personachat.model";
+import { initMongoDB } from "@/database/mongodb";
+import mongoose from "mongoose";
+
+export async function getPersonaChat(id: string) {
+  mongoose.connection.readyState === 1
+    ? console.log("Mongoose Connected")
+    : await initMongoDB();
+  const personaChat = await PersonaChat.findOne({ _id: id });
+
+  return personaChat;
+}
