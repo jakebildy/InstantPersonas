b5d18ee6aa396a89e9aabea5caa03296a5ef4b57
jakebildy <jbildy@uwo.ca>
2024-04-26 21:11:26 -0400

    :fire: fixed persona history

--- next/app/(server)/api/(persona-crud)/get-persona-history/route.ts
+++ next/app/(server)/api/(persona-crud)/get-persona-history/route.ts
@@ -1,5 +1,7 @@
 import { PersonaChat } from "@/app/(server)/models/personachat.model";
 import { PERSONA_TEST_HISTORY } from "./test";
+import { initMongoDB } from "@/database/mongodb";
+import mongoose from "mongoose";
 
 export async function GET(req: Request) {
 
@@ -7,6 +9,8 @@ export async function GET(req: Request) {
   const url = new URL(req.url)
 
   const userID = url.searchParams.get("id")
+
+  mongoose.connection.readyState === 1 ? console.log("Mongoose Connected") : await initMongoDB();
 
   // Get MongoDB PersonaChats where user matches the provided userID
   const personaChats = await PersonaChat.find({ user: userID });
