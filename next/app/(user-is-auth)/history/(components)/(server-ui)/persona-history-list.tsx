147b596b5ae8060570488dea4619c5bd6528be10
Zero <writemarcuslim@gmail.com>
2024-05-03 18:53:02 -0400

    chore(feedback): improved show feedback conditions

--- next/app/(user-is-auth)/history/(components)/(server-ui)/persona-history-list.tsx
+++ next/app/(user-is-auth)/history/(components)/(server-ui)/persona-history-list.tsx
@@ -41,41 +41,50 @@ export function PersonaHistoryList({}: {}) {
       checkSubscription();
 
       const checkAndShowFeedbackDialog = () => {
-        if (!user.user) return;
+        if (!user.user) return; // Ensure there is a user object
+
+        const currentDate = new Date();
+
+        // Function to get the date x days ago without mutating currentDate
+        const daysAgoInTime = (days: number) => {
+          let pastDate = new Date(currentDate.getTime());
+          pastDate.setDate(pastDate.getDate() - days);
+          return pastDate;
+        };
+
         // Check if user has generated more than 1 persona
         const personasGenerated = personachats.length;
         const personasGeneratedWithinThreshold = personasGenerated >= 1;
 
-        // Get current date
-        const currentDate = new Date();
-        // Function to get the date x days ago
-        const daysAgoInTime = (days: number) =>
-          new Date(currentDate.setDate(currentDate.getDate() - days));
-
-        // Check local storage to see if the feedback dialog has already been shown within the last day
+        // Check if the feedback dialog has been shown within the last day
         const feedbackShownDate = localStorage.getItem(
           LOCAL_STORAGE_CONFIG.feedback.shown
         );
         const feedbackNotRecentlyShown = feedbackShownDate
-          ? new Date(feedbackShownDate) <= daysAgoInTime(1)
+          ? new Date(feedbackShownDate) < daysAgoInTime(1)
           : true;
 
-        // If feedback has not been completed in the last 60 days, then condition is true
+        // Check if feedback has been completed within the last 60 days
         const feedbackCompletedDate = localStorage.getItem(
           LOCAL_STORAGE_CONFIG.feedback.completed
         );
         const feedbackNotRecentlyCompleted = feedbackCompletedDate
-          ? new Date(feedbackCompletedDate) <= daysAgoInTime(60)
+          ? new Date(feedbackCompletedDate) < daysAgoInTime(60)
           : true;
 
-        // If feedbackNotRecentlyCompleted show
-        // Else if feedbackNotRecentlyShown and personasGeneratedWithinThreshold show
+        // Logic to show the feedback dialog (starting at new user)
+        // ------------------------------
+        // If user has used the app (generated at least 1 persona), show the feedback dialog
+        // If user has seen the feedback dialog but clicked off it (didn't want to complete), don't show it again for 1 day (to prevent spamming the user and to get non spam / annoyed feedback)
+        // If user has completed the feedback dialog, don't show it again for 60 days
 
-        if (
-          feedbackNotRecentlyCompleted ||
-          (personasGeneratedWithinThreshold && feedbackNotRecentlyShown)
-        ) {
+        const showFeedback =
+          (personasGeneratedWithinThreshold && feedbackNotRecentlyShown) ||
+          feedbackNotRecentlyCompleted;
+
+        if (showFeedback) {
           setShowUserFeedbackDialog(true);
+          // Set / Update the date the feedback dialog was shown
           const currentDateFormatted = new Date().toISOString().slice(0, 10); // Format as 'YYYY-MM-DD'
           localStorage.setItem(
             LOCAL_STORAGE_CONFIG.feedback.shown,
