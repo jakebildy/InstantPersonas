983bd1dd17dd5c48dc56a97745a4ad9c27fbc253
Zero <writemarcuslim@gmail.com>
2024-05-09 19:03:12 -0400

    chore(auth): rm unused import

--- next/app/(auth)/auth/page.tsx
+++ next/app/(auth)/auth/page.tsx
@@ -1,6 +1,5 @@
 import Authenticate, { AuthFallback } from "@/components/auth/stytch-auth";
 import { Suspense } from "react";
-import BarLoader from "react-spinners/BarLoader";
 
 export default function AuthenticatePage() {
   return (
