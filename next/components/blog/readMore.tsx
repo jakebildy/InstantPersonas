0e25947d474fc7693e1a5cba1eed351de4433131
jakebildy <jbildy@uwo.ca>
2024-05-09 11:17:13 -0700

    :fire: fixed build error

--- next/components/blog/readMore.tsx
+++ next/components/blog/readMore.tsx
@@ -10,18 +10,16 @@ export function ReadMore({ currentSlug }: { currentSlug: string }) {
       <div className="grid gap-8 lg:grid-cols-2 mx-10 mt-10">
         {BLOG_POSTS.filter((post) => post.slug != currentSlug).map(
           (post, i) => (
-            <div>
-              <ArticleCard
-                post={post}
-                key={post.slug}
-                category={post.category}
-                className={
-                  BLOG_POSTS.length % 2 !== 0 && i == BLOG_POSTS.length - 1
-                    ? "lg:col-span-2"
-                    : ""
-                }
-              />
-            </div>
+            <ArticleCard
+              post={post}
+              key={post.slug}
+              category={post.category}
+              className={
+                BLOG_POSTS.length % 2 !== 0 && i == BLOG_POSTS.length - 1
+                  ? "lg:col-span-2"
+                  : ""
+              }
+            />
           )
         )}
       </div>
