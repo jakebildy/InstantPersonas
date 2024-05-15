ff83d5e7369afcdaf085f3af0f30635c05df81ab
Zero <writefragile@gmail.com>
2024-05-15 11:16:07 -0400

    chore(post-finder): demo video

--- next/app/(public)/tools/guest-post-finder/page.tsx
+++ next/app/(public)/tools/guest-post-finder/page.tsx
@@ -8,6 +8,8 @@ import { useStytchUser } from "@stytch/nextjs";
 import { usePostHog } from "posthog-js/react";
 import { useEffect, useState } from "react";
 import { ArticleCard, BLOG_POSTS } from "../../blog/page";
+import * as SelectPersonaDemoGif from "@/public/persona-select-demo.gif";
+import Image from "next/image";
 
 export default function GuestPostOpportunityFinder({}: {}) {
   const [personaString, setPersonaString] = useState<string>("");
@@ -66,13 +68,6 @@ export default function GuestPostOpportunityFinder({}: {}) {
         <div />
       )}
       <div className="flex flex-col items-center h-full w-full">
-        {userIsSubscribed ? (
-          <PersonaSelectFromHistorySidebar
-            selectedPersonas={selectedPersonas}
-            setSelectedPersonas={setSelectedPersonas}
-            className="absolute top-4 right-4"
-          />
-        ) : null}
         <h1 className="text-3xl text-gray-700 text-center pt-10 font-bold">
           Guest Post Opportunity Finder
         </h1>
@@ -82,29 +77,49 @@ export default function GuestPostOpportunityFinder({}: {}) {
           Find the best opportunities for your niche here.
         </h2>
 
-        <div className="flex flex-col items-center w-full mb-10">
+        <div className="flex flex-col items-center w-full mb-10 gap-2">
           {userIsSubscribed ? (
-            <section className="border border-gray-300 rounded-md w-1/2 min-h-[400px] bg-white p-2 flex flex-col gap-2">
-              {selectedPersonas.map((persona, i) => (
-                <SelectArchetypeWidget
-                  key={i}
-                  archetype={persona}
-                  isSelected={true}
-                  onSelect={() => {
-                    setSelectedPersonas((prevSelectedPersonas) => [
-                      ...prevSelectedPersonas,
-                      persona,
-                    ]);
-                  }}
-                  onDeselect={() => {
-                    setSelectedPersonas((prevSelectedPersonas) =>
-                      prevSelectedPersonas.filter(
-                        (activePersona) => activePersona !== persona
-                      )
-                    );
-                  }}
-                />
-              ))}
+            <PersonaSelectFromHistorySidebar
+              selectedPersonas={selectedPersonas}
+              setSelectedPersonas={setSelectedPersonas}
+              className="xl:absolute top-4 right-4"
+            />
+          ) : null}
+          {userIsSubscribed ? (
+            <section className="border border-gray-300 rounded-md w-1/2 bg-white p-2 flex flex-col gap-2">
+              {selectedPersonas.length > 0 ? (
+                selectedPersonas.map((persona, i) => (
+                  <SelectArchetypeWidget
+                    key={i}
+                    archetype={persona}
+                    isSelected={true}
+                    onSelect={() => {
+                      setSelectedPersonas((prevSelectedPersonas) => [
+                        ...prevSelectedPersonas,
+                        persona,
+                      ]);
+                    }}
+                    onDeselect={() => {
+                      setSelectedPersonas((prevSelectedPersonas) =>
+                        prevSelectedPersonas.filter(
+                          (activePersona) => activePersona !== persona
+                        )
+                      );
+                    }}
+                  />
+                ))
+              ) : (
+                <div className="rounded-md overflow-hidden h-full w-full">
+                  <Image
+                    src={SelectPersonaDemoGif}
+                    alt={
+                      "Select Personas or Enter Details to Generate Guest Posts"
+                    }
+                    width={800}
+                    height={600}
+                  />
+                </div>
+              )}
             </section>
           ) : null}
           <label className="text-sm text-gray-700 my-2">
