e5f57395b4fc5df10f12ad16a0d6492ac0993475
jakebildy <jbildy@uwo.ca>
2024-05-15 14:54:33 -0700

    :fire: set up reactflow

--- next/components/toolfolio/topical-authority-map/topical-authority-map.tsx
+++ next/components/toolfolio/topical-authority-map/topical-authority-map.tsx
@@ -1,5 +1,69 @@
 // hello world
 
+import ReactFlow, {
+  Background,
+  BackgroundVariant,
+  MarkerType,
+} from "reactflow";
+
+import "reactflow/dist/style.css";
+import TopicalLinkNode from "./topical-link-node";
+
+const initialNodes = [
+  {
+    id: "node-1",
+    type: "topicalLink",
+    position: { x: 0, y: 400 },
+    data: { value: 123 },
+  },
+  {
+    id: "node-2",
+    type: "topicalLink",
+    position: { x: 0, y: 0 },
+    data: { value: 123 },
+  },
+  {
+    id: "node-3",
+    type: "topicalLink",
+    position: { x: 300, y: 400 },
+    data: { value: 123 },
+  },
+];
+const initialEdges = [
+  {
+    id: "e1-2",
+    source: "node-2",
+    target: "node-1",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 20,
+      height: 20,
+      color: "#FF0072",
+    },
+    style: {
+      strokeWidth: 2,
+      stroke: "#FF0072",
+    },
+  },
+  {
+    id: "e2-3",
+    source: "node-2",
+    target: "node-3",
+    markerEnd: {
+      type: MarkerType.ArrowClosed,
+      width: 20,
+      height: 20,
+      color: "#FF0072",
+    },
+    style: {
+      strokeWidth: 2,
+      stroke: "#FF0072",
+    },
+  },
+];
+
+const nodeTypes = { topicalLink: TopicalLinkNode };
+
 export function TopicalAuthorityMap() {
   return (
     <div
