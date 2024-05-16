cc33e9f3445f1454f21bb8693b9bcd93eb7abe03
jakebildy <jbildy@uwo.ca>
2024-05-15 19:18:01 -0700

    :fire: prompt tweaking

--- next/app/(server)/api/(ai-tools)/topical-authority/prompt.ts
+++ next/app/(server)/api/(ai-tools)/topical-authority/prompt.ts
@@ -1,12 +1,13 @@
 export const TOPICAL_AUTHORITY_PROMPT = ({
-  category,
+  personas,
 }: {
-  category: string;
+  personas: string;
 }) => `
-give me 30 semantically relevant but unique topics under the main category of ${category}, and for each,
+give me 5 semantically relevant but unique topics under a main category that these personas would be interested in ${personas}, and for each,
 give me 10 different variations of the topic that each address a different search intent. 
 Make it in table format with the following columns: 
   column 1: The unique semantically related topic, 
   column 2: the different variations on it, 
   column 3: an intriguing, clickbait style title.
+  Return nothing but the table in the form of comma separated values.
 `;
