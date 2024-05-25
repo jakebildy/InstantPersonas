bfbbb4ee0c3b6daaefdf78ed808179d69e46cd06
Zero <writemarcuslim@gmail.com>
2024-05-24 23:59:48 -0400

    feat(SocialPreviewIntegrationShowcase): added

--- next/app/test/page.tsx
+++ next/app/test/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { TemplateEditView } from "@/components/toolfolio/share-preview-optimizer/image-template-editor/image-template-edit-view";
+import { SocialPreviewIntegrationShowcase } from "@/components/toolfolio/share-preview-optimizer/integration-showcase";
 import React, { useEffect, useState } from "react";
 
 type Props = {};
@@ -8,16 +9,7 @@ type Props = {};
 export default function PageTest({}: Props) {
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen relative ">
-      <TemplateEditView
-        variant={"blue"}
-        initialUrl={""}
-        initialTitle={""}
-        initialDescription={""}
-        initialImage={""}
-        selectedTemplate={"Instant Personas Blog"}
-        setShowTemplateSelectModal={() => {}}
-        setVariant={() => {}}
-      />
+      <SocialPreviewIntegrationShowcase />
     </div>
   );
 }
