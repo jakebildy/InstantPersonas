7ff72ebab0a5a4f5df45aa4ffa47c60d354ec114
Zero <writemarcuslim@gmail.com>
2024-06-02 22:04:54 -0400

    improved ai system

--- next/app/(server)/ai/persona-chat-ai/initial-ai-state.ts
+++ next/app/(server)/ai/persona-chat-ai/initial-ai-state.ts
@@ -4,7 +4,11 @@ import { AIState } from "../../models/persona-ai.model";
 export const PERSONA_CHAT_INITIAL_AI_STATE: AIState = {
   chatId: nanoid(),
   messages: [],
-  suggestedMessages: [],
+  suggestedMessages: [
+    "Tell me about Instant Personas!",
+    "How do I use your Tools?",
+    "What specifically should I say about my business?",
+  ],
   business: "",
   personas: [],
   targetProblem: "",
