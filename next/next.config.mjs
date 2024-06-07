44082ea773530e6e004ceb78824f1f1b32e3e25b
jakebildy <jbildy@uwo.ca>
2024-06-07 14:02:01 -0700

    :fire: removing webpack config

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -9,6 +9,31 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  // webpack: (config, { isServer }) => {
+  //   if (isServer) {
+  //     // Add null-loader to prevent SSR of the `tiptap` package
+  //     config.module.rules.push({
+  //       test: /@tiptap/,
+  //       use: 'null-loader',
+  //     },
+  //     {
+  //       test: /@tiptap\/extension-image/,
+  //       use: 'null-loader',
+  //     },
+  //     {
+  //       test: /@tiptap\/extension-resize-image/,
+  //       use: 'null-loader',
+  //     },
+  //     {
+  //       test: /tiptap-markdown/,
+  //       use: 'null-loader',
+  //     });
+  //   }
+
+  //   // Add any additional custom Webpack configurations here
+
+  //   return config;
+  // },
 };
 
 export default nextConfig;
