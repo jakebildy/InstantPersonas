ce5d585fea9cdaa22b174b3b27cece8f2edc39b2
Zero <writemarcuslim@gmail.com>
2024-06-11 19:34:54 -0400

    fix(ai): gpt route imports

--- next/app/(server)/api/(ai-tools)/autocomplete/route.ts
+++ next/app/(server)/api/(ai-tools)/autocomplete/route.ts
@@ -1,9 +1,5 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import axios from "axios";
+import { ChatGPT } from "@/app/(server)/ai/persona-chat-ai/utils/gpt";
 import { NextResponse } from "next/server";
-import { ChatGPT, GPT4 } from "@/app/(server)/ai/gpt";
-
 
 export async function POST(req: Request) {
   if (req.body) {
@@ -14,15 +10,15 @@ export async function POST(req: Request) {
       const personas = body.personas;
 
       const systemMessage =
-        "Keep writing this (at a grade 9 reading level, continue exactly from where the text ends. This is a blog post): " + body.text; 
+        "Keep writing this (at a grade 9 reading level, continue exactly from where the text ends. This is a blog post): " +
+        body.text;
       const chatResponse = await ChatGPT(systemMessage);
 
       console.log("response: " + chatResponse.text.trim());
 
-     
       return NextResponse.json({
         response: chatResponse.text.trim(),
       });
-    } 
+    }
   }
 }
--- next/app/(server)/api/(ai-tools)/google-keywords-blog/route.ts
+++ next/app/(server)/api/(ai-tools)/google-keywords-blog/route.ts
@@ -1,9 +1,6 @@
-import OpenAI from "openai";
-import { OpenAIStream, StreamingTextResponse } from "ai";
 import axios from "axios";
 import { NextResponse } from "next/server";
-import { ChatGPT, GPT4 } from "@/app/(server)/ai/gpt";
-
+import { ChatGPT, GPT4 } from "@/app/(server)/ai/persona-chat-ai/utils/gpt";
 
 export async function POST(req: Request) {
   if (req.body) {
@@ -21,7 +18,6 @@ export async function POST(req: Request) {
       const chatResponse = await GPT4(systemMessage);
 
       console.log("response: " + chatResponse.text.trim());
-
 
       // const keywordValues = await axios.post(
       //   'https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live',
@@ -42,19 +38,22 @@ export async function POST(req: Request) {
       // )
       let keywordResults: any = [];
       // keywordResults = keywordValues.data.tasks[0].result;
-       
-      // map them to objects where each object contains keyword, 
-      // search_volume, cpc, competition are null
-      keywordResults = chatResponse.text.trim().split(/,\s*/).map((keyword: string) => {
-        return {
-          keyword: keyword,
-          search_volume: null,
-          cpc: null,
-          competition: null
-        }
-      })
 
-      console.log( "keywordResults: ", keywordResults)
+      // map them to objects where each object contains keyword,
+      // search_volume, cpc, competition are null
+      keywordResults = chatResponse.text
+        .trim()
+        .split(/,\s*/)
+        .map((keyword: string) => {
+          return {
+            keyword: keyword,
+            search_volume: null,
+            cpc: null,
+            competition: null,
+          };
+        });
+
+      console.log("keywordResults: ", keywordResults);
 
       return NextResponse.json({
         keywords: keywordResults,
@@ -73,31 +72,29 @@ export async function POST(req: Request) {
 
       console.log("response: " + chatResponse.text.trim());
 
-
-
       const keywordValues = await axios.post(
-        'https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live',
-        [{
-          location_name: 'United States',
-          language_name: 'English',
-          keywords: chatResponse.text.trim().split(/,\s*/)
-        }],
+        "https://api.dataforseo.com/v3/keywords_data/google_ads/search_volume/live",
+        [
+          {
+            location_name: "United States",
+            language_name: "English",
+            keywords: chatResponse.text.trim().split(/,\s*/),
+          },
+        ],
         {
           auth: {
             username: process.env.DATA_FOR_SEO_USERNAME!,
-            password: process.env.DATA_FOR_SEO_PASSWORD!
+            password: process.env.DATA_FOR_SEO_PASSWORD!,
           },
           headers: {
-            'content-type': 'application/json'
-          }
-        }
-      )
+            "content-type": "application/json",
+          },
+        },
+      );
       let keywordResults: any = [];
       keywordResults = keywordValues.data.tasks[0].result;
 
-      console.log( "keywordResults: ", keywordResults)
-
-      
+      console.log("keywordResults: ", keywordResults);
 
       return NextResponse.json({
         keywords: keywordResults,
