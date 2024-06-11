c3c42675e466a8df1f627694abedf4f208a8881b
jakebildy <jbildy@uwo.ca>
2024-06-11 15:33:28 -0700

    :fire: fixed suggested messages

--- next/app/(server)/ai/persona-chat-ai/initial-ai-state.ts
+++ next/app/(server)/ai/persona-chat-ai/initial-ai-state.ts
@@ -4,11 +4,7 @@ import { AIState } from "../../models/persona-ai.model";
 export const PERSONA_CHAT_INITIAL_AI_STATE: AIState = {
   chatId: nanoid(),
   messages: [],
-  suggestedMessages: [
-    "Tell me about Instant Personas!",
-    "How do I use your Tools?",
-    "What specifically should I say about my business?",
-  ],
+  suggestedMessages: [],
   business: "",
   personas: [],
   targetProblem: "",
