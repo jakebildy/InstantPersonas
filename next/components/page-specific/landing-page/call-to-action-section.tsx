9adb7d8130a934e9ea90e2cb25ab8fe3899cd853
jakebildy <jbildy@uwo.ca>
2024-06-11 21:14:04 -0700

    :fire: ui tweak

--- next/components/page-specific/landing-page/call-to-action-section.tsx
+++ next/components/page-specific/landing-page/call-to-action-section.tsx
@@ -68,7 +68,7 @@ export default function CallToActionSection() {
             onClick={() => {
               window.location.href = "/register";
             }}
-            className="px-8 py-2 rounded-full bg-gradient-to-b from-green-500 to-green-600 text-white focus:ring-2 focus:ring-green-400 hover:shadow-xl transition duration-200"
+            className="px-8 py-2 rounded-full bg-green-600 text-white font-jost focus:ring-2 focus:ring-green-400 hover:shadow-xl transition duration-200"
           >
             Get 3 days FREE
           </button>
