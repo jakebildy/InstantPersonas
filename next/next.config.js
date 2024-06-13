136a0ebc8de783c8d46940764a1bbdc8e12098c7
Zero <writefragile@gmail.com>
2024-06-12 22:51:10 -0400

    pnpm i

--- next/next.config.js
+++ next/next.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
@@ -6,9 +6,9 @@ module.exports = {
     // Polyfill for Node.js core modules
     config.resolve.fallback = {
       ...config.resolve.fallback,
-      os: require.resolve('os-browserify/browser'),
-      zlib: require.resolve('browserify-zlib'),
-      stream: require.resolve('stream-browserify'),
+      os: require.resolve("os-browserify/browser"),
+      zlib: require.resolve("browserify-zlib"),
+      stream: require.resolve("stream-browserify"),
     };
 
     // Return the modified config
