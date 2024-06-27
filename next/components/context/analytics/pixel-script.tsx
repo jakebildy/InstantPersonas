4422dd5a3e6c930090a7a4afef161c8000fe84ee
jakebildy <jbildy@uwo.ca>
2024-06-26 19:42:00 -0700

    :fire: removing page view tracking from pixel script

--- next/components/context/analytics/pixel-script.tsx
+++ next/components/context/analytics/pixel-script.tsx
@@ -15,7 +15,6 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '800239702201831');
-fbq('track', 'PageView');
             `,
       }}
     />
